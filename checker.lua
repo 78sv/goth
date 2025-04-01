e4df87b5f9aca80ab2c10a2e7e49a4d49a585c13
78sv <rostinebr@gmail.com>
2025-04-01 18:27:05 -0500

    Updated 'checker.lua'
    Edited to make sure user ran before running actual code.

--- checker.lua
+++ checker.lua
@@ -1,12 +1,20 @@
--- Loading Config
-local deloadIfLoaded = false --[[ False = Cancel Execution
-								  True = Overrite Current Execution ]] 
+-- GOTH Title
+print("GOTH - @87sv")
+warn("Don't execute twice! It's loading!")
 
--- GOTH Initialize
+-- GOTH Check
 if game:GetService("Workspace"):FindFirstChild("GOTH Parent") then
 	if deloadIfLoaded then
+		print("[GOTH] Canceled Execution.")
 		return -- Stop Execution
-	else
-		game:GetService("Workspace"):FindFirstChild("GOTH Parent"):Destroy()
 	end
 end
+
+game:GetService("Workspace"):FindFirstChild("GOTH Parent"):Destroy()
+print("[GOTH] Destroyed GOTH Parent.")
+
+-- GOTH Initializer
+local GOTHParent = Instance.new("Folder") -- Create GOTH Instance
+-- Create Folder Insight
+GOTHParent.Name = "GOTH Parent"
+GOTHParent.Parent = game:GetService("Workspace")
